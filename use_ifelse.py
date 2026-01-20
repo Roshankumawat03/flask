ac6531e61ebec34a42e03862c3eb81e53b1670ad
Roshan Kumawat <roshankumawat529@gmail.com>
2026-01-20 14:37:55 +0530

    Jinja Templating - If Else and Args Managment

---
+++ use_ifelse.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from data import user_data
+user_data = user_data["users"]
+
+app = Flask(__name__)
+
+
+@app.route("/check_div/<int:number>")
+def div_by_5_and_3(number):
+    return render_template("check_div.html", number=number)
+
+
+@app.route("/list")
+def list_users():
+    return render_template("list_users.html", title="List Users", user_list=user_data)
+
+
+@app.route("/watch")
+def get_request_data():
+    v = request.args.get("v")
+    return render_template("yt.html", title="Play Video", v=v)
