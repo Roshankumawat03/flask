9d5c9dbfa82cb85be5362e90809951184e1444d1
Roshan Kumawat <roshankumawat529@gmail.com>
2026-02-02 20:41:34 +0530

    Update, Create and Delete in DB

--- learn_db.py
+++ learn_db.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///roshan.db"
 
 db = SQLAlchemy(app)
 
@@ -17,6 +17,47 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
 
+u1 = User(is_admin=False, username="rohan", email="rohan@gmail.com", password="12345")
+u2 = User(is_admin=False, username="aman", email="aman@gmail.com", password="12345")
+u3 = User(is_admin=False, username="naman", email="naman@gmail.com", password="12345")
+u4 = User(is_admin=True, username="aryan", email="aryan@gmail.com", password="12345")
+u5 = User(is_admin=True, username="sahil", email="sahil@gmail.com", password="12345")
+u6 = User(is_admin=True, username="sohan", email="sohan@gmail.com", password="12345")
+   
+
 with app.app_context():
     db.create_all()
+    db.session.add(u1)
+    db.session.add(u2)
+    db.session.add(u3)
+    db.session.add(u4)
+    db.session.add(u5)
+    db.session.add(u6)
+    db.session.commit()
 
+
+with app.app_context():
+    user = User.query.get(1)
+    user.username = "rahul"
+    user.email = "rk@gmail.com"
+    db.session.commit()
+
+
+with app.app_context():
+    user = User.query.get(2)
+    db.session.delete(user)
+    db.session.commit() 
+
+with app.app_context():
+    user = User.query.get(3)
+    db.session.commit()
+    print(user.username)
+
+
+
+with app.app_context():
+    db.create_all()
+    for x in range(100):
+        a = Mytable()
+        db.session.add(a)
+    db.session.commit()
